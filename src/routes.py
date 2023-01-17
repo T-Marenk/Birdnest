cf0945c3ba9af0f20caa0fb9ca4b0d9b359a2cef
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-01-17 11:59:55 +0200

    Updater in flask

--- src/routes.py
+++ src/routes.py
@@ -1,14 +1,21 @@
-from app import app
+from threading import Timer
 from flask import render_template, redirect, request, jsonify
+from app import app
 from src.services.drone_service import drone_service
+
+def updater(interval):
+    Timer(interval, updater, [interval]).start()
+    drone_service._update_violators()
+
+updater(1)
 
 @app.route("/")
 def index():
-    violators = drone_service.update_violators()
+    violators = drone_service.get_violators()
     return render_template("index.html", violators=violators)
 
 @app.route("/update", methods=['POST'])
 def update():
      
-    violators = drone_service.update_violators()
+    violators = drone_service.get_violators()
     return render_template("section.html", violators=violators)
